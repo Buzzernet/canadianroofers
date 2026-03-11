0c4c197f6cb0e0433bb49deb1c14fe375c156619
nanirama <nani.rama1982@gmail.com>
2026-03-11 07:32:29 +0530

    Add Microsoft-uet script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
             gtag('config', 'AW-804750091');
           `}
         </Script>
+        {/* Microsoft UET (Bing Ads) */}
+        <Script id="microsoft-uet" strategy="afterInteractive">
+          {`
+            (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"247016986", enableAutoSpaTracking: true};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","https://bat.bing.com/bat.js","uetq");
+          `}
+        </Script>
         {children}
       </body>
     </html>
