2687b29adbb9a973a08d8592786933291a0ceb69
nanirama <nani.rama1982@gmail.com>
2026-04-28 10:01:52 +0530

    Add LandingA as default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 // src/app/page.tsx
-import { headers } from "next/headers";
+//import { headers } from "next/headers";
 import LandingPageA from "@/components/landingpageA";
 import LandingPageB from "@/components/landingpageB";
 
 export const dynamic = "force-dynamic"; // always run on each request
 
 export default async function Home() {
-  const headersList = await headers();
-  const variant = headersList.get("x-ab-variant") ?? "A";
+  //onst headersList = await headers();
+  //const variant = headersList.get("x-ab-variant") ?? "A";
 
-  return variant === "B" ? <LandingPageB /> : <LandingPageA />;
+  return <LandingPageA />;
 }
